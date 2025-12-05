e7ffe0929b2a71835cb5974e2e045a01592af193
yc08 <yencheng.wu.yc@gmail.com>
2025-12-05 09:51:15 +0800

    able to calculate + - * / %

--- calculator.cpp
+++ calculator.cpp
@@ -4,14 +4,68 @@
 #include<sstream>
 #include"BigInt.h"
 #define bigint Bigint<256>
+void printbig(const std::vector<bigint>&vec){
+    for(int i=0;i<vec.size();++i){
+        std::cout<<vec[i].num()<<" ";
+    }
+    std::cout<<"\n";
+}
+template<typename T>
+void printVec(const std::vector<T>&vec){
+    for(const T &v:vec){
+        std::cout<<v<<" ";
+    }
+    std::cout<<"\n";
+}
 
-bigint cal(std::vector<std::string>&arr){
+bigint cal(bigint x,bigint y,int op){
+    switch(op){
+        case 0:
+            return x*y;
+        case 1:
+            return x/y;
+        case 2:
+            return x%y;
+        case 3:
+            return x+y;
+        case 4:
+            return x-y;
+        default:
+            return bigint(0);
+    }
+}
+
+bigint calculator(std::vector<std::string>&arr){
     size_t n=arr.size();
-    std::vector<char>ops(n,' ');
+    std::vector<int>ops(n,0);
     std::vector<bigint>nums(n);
-    int numCount=0,opCount=0;
+    std::vector<char>precedence={'*','/','%','+','-','E'};
+    int numCount=-1,opCount=-1;
     for(const auto &token:arr){
-        
+        int op=-1;
+        for(int i=0;i<precedence.size();++i){
+            if(token[0]==precedence[i]&&token.size()==1){
+                op=i;
+                break;
+            }
+        }
+        if(op!=-1){
+            if(ops[opCount]<op){
+                while(opCount >= 0 && ops[opCount] <= op){
+                    char oper=ops[opCount--];
+                    bigint b=nums[numCount--];
+                    bigint a=nums[numCount];
+                    bigint res;
+                    nums[numCount]=cal(a,b,oper);
+                }
+            }
+            ops[++opCount]=op;
+        }
+        else{
+            nums[++numCount]=bigint(token);
+        }
+        // printbig(nums);
+        // printVec(ops);
     }
     return nums[0];
 }
@@ -26,6 +80,7 @@ int main(){
         if(token.empty()) continue;
         arr.push_back(token);
     }
-    std::cout<<cal(arr).num()<<"\n";
+    arr.push_back("E");
+    std::cout<<calculator(arr).num()<<"\n";
     return 0;
 }
